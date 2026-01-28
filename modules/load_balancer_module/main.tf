8bb228df31ac96ee9161986b1e5d500d4531f446
soso <mingi371@naver.com>
2026-01-28 16:27:25 +0900

    commit

--- modules/load_balancer_module/main.tf
+++ modules/load_balancer_module/main.tf
@@ -15,7 +15,7 @@ resource "aws_lb" "tf_alb" {
 // 대상 그룹 생성
 resource "aws_lb_target_group" "tf_tg" {
   name     = "${var.pjt_name}-tg"
-  port     = 80
+  port     = 3000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
@@ -32,7 +32,7 @@ resource "aws_lb_target_group" "tf_tg" {
 // Listener 생성
 resource "aws_lb_listener" "tf_listener" {
   load_balancer_arn = aws_lb.tf_alb.arn
-  port              = 80
+  port              = 3000
   protocol          = "HTTP"
 
   default_action {
